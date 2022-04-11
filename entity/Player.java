9e9af40daf009afb794791bdc1d93c0ce6ece26b
rinaanir03 <101147688+rinaanir03@users.noreply.github.com>
2022-04-11 22:23:51 +0300

    Add files via upload

---
+++ entity/Player.java
@@ -0,0 +1,130 @@
+package entity;
+
+import main.GamePanel;
+import main.KeyHandler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Player extends Entity
+{
+    GamePanel gp;
+    KeyHandler keyH;
+
+    public Player (GamePanel gp, KeyHandler keyH)
+    {
+        this.gp   = gp;
+        this.keyH = keyH;
+
+        setDefaultValues();
+        getPlayerImage();
+    }
+
+    public void setDefaultValues()
+    {
+        x     = 100;
+        y     = 100;
+        speed = 4;
+
+        direction = "down";
+    }
+
+    public void getPlayerImage()
+    {
+        try
+        {
+            up1    = ImageIO.read(getClass().getResourceAsStream("/res/up1.png"));
+            up2    = ImageIO.read(getClass().getResourceAsStream("/res/up2.png"));
+            down1  = ImageIO.read(getClass().getResourceAsStream("/res/down1.png"));
+            down2  = ImageIO.read(getClass().getResourceAsStream("/res/down2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/res/right1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/res/right2.png"));
+            left1  = ImageIO.read(getClass().getResourceAsStream("/res/left1.png"));
+            left2  = ImageIO.read(getClass().getResourceAsStream("/res/left2.png"));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void update()
+    {
+        if(keyH.upPressed == true)
+        {
+            y         -= speed;
+            direction = "up";
+        }
+
+        else if(keyH.downPressed == true)
+        {
+            y         += speed;
+            direction = "down";
+        }
+        else if(keyH.rightPressed == true)
+        {
+            x         += speed;
+            direction = "right";
+        }
+        else if(keyH.leftPressed == true)
+        {
+            x         -= speed;
+            direction = "left";
+        }
+
+        spriteCounter++;
+        if(spriteCounter  > 15){
+            if(spriteNum  == 1){
+                spriteNum = 2;
+            }
+            else if (spriteNum == 2){
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
+        }
+    }
+
+    public void draw(Graphics2D g2)
+    {
+        BufferedImage image = null;
+
+        switch(direction)
+        {
+            case "up":
+                if (spriteNum == 1){
+                    image = up1;
+                }
+                if (spriteNum == 2){
+                    image = up2;
+                }
+                break;
+            case "down":
+                if (spriteNum == 1){
+                    image = down1;
+                }
+                if (spriteNum == 2){
+                    image = down2;
+                }
+                break;
+            case "right":
+                if (spriteNum == 1){
+                    image = right1;
+                }
+                if (spriteNum == 2){
+                    image = right2;
+                }
+                break;
+            case "left":
+                if (spriteNum == 1){
+                    image = left1;
+                }
+                if (spriteNum == 2){
+                    image = left2;
+                }
+                break;
+        }
+        g2.drawImage(image,x, y, gp.tileSize, gp.tileSize, null);
+    }
+}
